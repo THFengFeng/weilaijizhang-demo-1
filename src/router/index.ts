fbbe63e43fbd07afcbd0e2fcaadafba2673f1363
fengfeng <632930757@qq.com>
2020-07-17 09:36:17 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import Vue from "vue";
-import VueRouter, {RouteConfig} from "vue-router";
+import VueRouter from "vue-router";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
-// @ts-ignore
-const routes: Array<RouteConfig> = [
+
+const routes = [
   {
     path: "/",
     redirect: "/money"
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics
+  },
+  {
+    path: "*",
+    component: NotFound
   },
   //{
   // path: '/',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        当前路径不存在，输入正确路径
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "NotFound"
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
