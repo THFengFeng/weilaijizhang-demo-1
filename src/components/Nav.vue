49f164df3a99688cc657b2d3e1d1305005eddfb2
fengfeng <632930757@qq.com>
2020-07-23 15:19:09 +0800

    模块化Money.vue分成四个子组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,63 @@
 <template>
     <div class="nav ">
-        <router-link to="/money">记账</router-link>
+
+        <router-link active-class="selected" class="item" to="/money">
+            <Icon name="money"/>
+            记账
+        </router-link>
         |
-        <router-link to="/labels">标签</router-link>
+        <router-link active-class="selected" class="item" to="/labels">
+            <Icon name="label"/>
+            标签
+        </router-link>
         |
-        <router-link to="/statistics">统计</router-link>
+        <router-link active-class="selected" class="item" to="/statistics">
+            <Icon name="statistics"/>
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+
     export default {
         name: "Nav"
     };
 </script>
 
 <style lang="scss" scoped>
+    @import "../assets/style/helper.scss";
+
     .nav {
-        border: 1px solid red;
+        @extend %outerShadow;
+        display: flex;
+
+        flex-direction: row;
+        font-size: 12px;
+
+        > .item {
+            padding: 2px 0;
+            width: 33.33333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+
+            .icon {
+                width: 32px;
+                height: 32px;
+
+            }
+        }
+
+        > .item.selected {
+            color: $color-highlight;
+
+            .icon {
+                fill: red;
+                //用fill填充颜色才能让图标变色
+            }
+        }
     }
+
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,28 @@
 <template>
-    <Layout>
-        <p>Labels.vue</p>
-    </Layout>
+    <div class="layout-wrapper">
+        <div :class="contentClass" class="content">
+            <slot/>
+        </div>
+        <Nav/>
+    </div>
 </template>
+
 <script lang="ts">
     export default {
+        props: ["contentClass"],
         name: "Labels"
     };
 </script>
+
+<style lang="scss" scoped>
+    .layout-wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+
+    .content {
+        overflow: auto;
+    }
+
+</style>
