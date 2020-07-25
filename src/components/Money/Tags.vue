40abfa26b01d705f4aa68be1a799fd36cd4b5e13
fengfeng <632930757@qq.com>
2020-07-25 22:28:16 +0800

    删减

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -15,34 +15,7 @@
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import {Component, Prop} from "vue-property-decorator";
 
-    @Component
-    export default class Tags extends Vue {
-
-        @Prop() readonly dataSource: string[] | undefined;
-        selectedTags: string[] = [];
-
-        toggle(tag: string) {
-            const index = this.selectedTags.indexOf(tag);
-            if (index >= 0) {
-                this.selectedTags.splice(index, 1);
-            } else {
-                this.selectedTags.push(tag);
-            }
-        }
-
-        create() {
-            const name = window.prompt("请输入标签名");
-            if (name === "") {
-                window.alert("标签名不能为空");
-            } else if (this.dataSource) {
-                this.$emit("update:dataSource",
-                    [...this.dataSource, name]);
-            }
-        }
-    }
 </script>
 
 <style lang="scss" scoped>
