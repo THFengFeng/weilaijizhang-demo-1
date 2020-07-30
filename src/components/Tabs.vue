3ca98239693ba68eedfcb5a432dd156d6232317a
fengfeng <632930757@qq.com>
2020-07-30 14:13:23 +0800

    添加 height 示例

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -19,6 +19,8 @@
         readonly value!: string;
         @Prop(String)
         classPrefix?: string;
+        @Prop({type: String, default: '64px'})
+        height!: string;
 
         liClass(item: DataSourceItem) {
             return {
