dd0506f905e82e1f37315f88f828bdd0094c2e9c
tonghaifeng <632930757@qq.com>
2020-08-02 21:11:09 +0800

    deploy

---
+++ precache-manifest.d8e8c240ffe017702336b0a20a6fc9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6d547650042e7296b6",
+    "url": "/weilaijizhang-demo-1-website/css/app.f1996d5e.css"
+  },
+  {
+    "revision": "1fed9defd88316eefe5e919d86d140b9",
+    "url": "/weilaijizhang-demo-1-website/index.html"
+  },
+  {
+    "revision": "fc6d547650042e7296b6",
+    "url": "/weilaijizhang-demo-1-website/js/app.abd45867.js"
+  },
+  {
+    "revision": "e4966484ca1307c6a9a1",
+    "url": "/weilaijizhang-demo-1-website/js/chunk-vendors.e631fa29.js"
+  },
+  {
+    "revision": "c85dfb4aeda956607145765b7ef84a12",
+    "url": "/weilaijizhang-demo-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weilaijizhang-demo-1-website/robots.txt"
+  }
+]);
